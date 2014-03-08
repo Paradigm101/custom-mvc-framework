7412c7adbbbfc59e1e5e3c6c4dd3dcf32b62e7d8
Paradigm101 <lthiery2003@gmail.com>
2014-03-07 20:24:13 -0500

    bootstrap demo: add features

--- includes/templates/bootstrapdemo_next_t.php
+++ includes/templates/bootstrapdemo_next_t.php
@@ -42,28 +42,63 @@
         <div class="col-12">
             <h2>More Features</h2>
             <p class="lead"><?= LOREM ?><p>
-        </div>
+        </div><!-- end col 12 -->
     </div><!-- end row featureHeading -->
 
 
     <div class="row" id="features">
         
         <div class="col-sm-4">
-            <div class="panel">
+            <div class="panel panel-default">
                 
-            </div>
-        </div>
-        
-        <div class="col-sm-4">
-            
-        </div>
-        
-        <div class="col-sm-4">
-            
-        </div>
-    </div><!-- end row features -->
+                <div class="panel-heading">
+                    <h3 class="panel-title">Markup with HTML5</h3>
+                </div><!-- end heading -->
 
-    
+                <div class="panel-body">
+                    <img src="includes/images/badge_html5.jpg" alt="HTML5" class="img-circle">
+                    <p>HTML5 is a markup language used for structuring and presenting content for the World Wide Web and a core technology of the Internet.</p>
+                    <a href="http://en.wikipedia.org/wiki/HTML5" target="_blank" class="btn btn-warning btn-block">HTML5 on wikipedia</a>
+                </div><!-- end body -->
+                
+            </div><!-- end panel -->
+        </div><!-- end feature -->
+        
+        <div class="col-sm-4">
+            <div class="panel panel-default">
+                
+                <div class="panel-heading">
+                    <h3 class="panel-title">Style with CSS3</h3>
+                </div><!-- end heading -->
+                
+                <div class="panel-body">
+                    <img src="includes/images/badge_css3.jpg" alt="CSS3" class="img-circle">
+                    <p>Cascading Style Sheets is a style sheet language used for describing the look and formatting of a document written in a markup language.</p>
+                    <a href="http://en.wikipedia.org/wiki/CSS" target="_blank" class="btn btn-danger btn-block">CSS3 on wikipedia</a>
+                </div><!-- end body -->
+
+            </div><!-- end panel -->
+        </div><!-- end feature -->
+        
+        <div class="col-sm-4">
+            <div class="panel panel-default">
+                
+                <div class="panel-heading">
+                    <h3 class="panel-title">Framework by Bootstrap 3</h3>
+                </div><!-- end heading -->
+                
+                <div class="panel-body">
+                    <img src="includes/images/badge_bootstrap.jpg" alt="Bootstrap 3" class="img-circle">
+                    <p>Bootstrap is a free collection of tools for creating websites and web applications. It contains HTML and CSS-based design templates.</p>
+                    <a href="http://en.wikipedia.org/wiki/Bootstrap_%28front-end_framework%29" target="_blank" class="btn btn-info btn-block">Bootstrap on wikipedia</a>
+                </div><!-- end body -->
+                
+            </div><!-- end panel -->
+        </div><!-- end feature -->
+        
+    </div><!-- end features -->
+
+
     <div class="row" id="moreInfo">
         
     </div><!-- end row moreInfo -->
